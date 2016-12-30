1f3a2d47458e4aedde96ff71b6d072bc4cbece9a
harmeetsingh0013 <harmeetsingh.0013@gmail.com>
2016-12-30 22:49:05 +0530

    Complete akka ComposingFuture WithActor WithActorNonBlocking examples

---
+++ src/main/java/com/knoldus/akka/future/WithActor.java
@@ -0,0 +1,40 @@
+package com.knoldus.akka.future;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+import akka.util.Timeout;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
+
+import java.util.concurrent.CompletableFuture;
+
+import static akka.pattern.PatternsCS.ask;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * Created by knoldus on 30/12/16.
+ */
+public class WithActor {
+
+    public static void main(String[] args) throws Exception {
+        final ActorSystem system = ActorSystem.apply("FaultTestingSystem");
+        final ActorRef a = system.actorOf(Props.create(A.class));
+        final CompletableFuture<Object> f = ask(a, "Hello", Timeout.apply(5, SECONDS))
+                .toCompletableFuture();
+        final String result = (String) f.get(5, SECONDS);
+        System.out.println(result);
+    }
+
+}
+
+class A extends AbstractActor {
+    @Override
+    public PartialFunction<Object, BoxedUnit> receive() {
+        return ReceiveBuilder
+                .matchAny(msg -> sender().tell("Hello", self()))
+                .build();
+    }
+}
---
+++ src/main/java/com/knoldus/akka/future/WithActorNonBlocking.java
@@ -0,0 +1,59 @@
+package com.knoldus.akka.future;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+import akka.util.Timeout;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
+
+import java.util.concurrent.CompletableFuture;
+
+import static akka.pattern.PatternsCS.ask;
+import static java.lang.System.out;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * Created by harmeet on 30/12/16.
+ */
+
+class A1 extends AbstractActor {
+    @Override
+    public PartialFunction<Object, BoxedUnit> receive() {
+        return ReceiveBuilder
+                .matchAny(msg -> sender().tell("Hello", self()))
+                .build();
+    }
+}
+
+class A2 extends AbstractActor {
+    @Override
+    public PartialFunction<Object, BoxedUnit> receive() {
+        return ReceiveBuilder
+                .match(String.class, msg -> out.println("got this " + msg))
+                .matchAny(msg -> out.println("got something else"))
+                .build();
+    }
+}
+
+public class WithActorNonBlocking {
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.apply("FaultTestingSystem");
+        final ActorRef a1 = system.actorOf(Props.create(A1.class));
+        final ActorRef a2 = system.actorOf(Props.create(A2.class));
+        final CompletableFuture<Object> f = ask(a1, "Hello", Timeout.apply(5, SECONDS))
+                .toCompletableFuture();
+
+        f.thenAccept(value -> {
+            out.println("success");
+            a2.tell(value, ActorRef.noSender());
+        })
+        .exceptionally(ex -> {
+            out.println("failed");
+            a2.tell(ex, ActorRef.noSender());
+            return null;
+        });
+    }
+}
