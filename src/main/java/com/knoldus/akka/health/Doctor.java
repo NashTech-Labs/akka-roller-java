16bfbcf445ef47e89a6a4fac3573e6c82ba91aa4
harmeetsingh0013 <harmeetsingh.0013@gmail.com>
2017-01-02 14:00:12 +0530

    Akka health sample application done

---
+++ src/main/java/com/knoldus/akka/health/Doctor.java
@@ -0,0 +1,56 @@
+package com.knoldus.akka.health;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.util.Timeout;
+import com.knoldus.akka.health.HeartLungMachine.PullBack;
+import com.knoldus.akka.health.OperationTheater.PassMeTheMachine;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import static akka.pattern.PatternsCS.ask;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * Created by knoldus on 2/1/17.
+ */
+public class Doctor {
+    public static void main(String[] args) throws Exception {
+        final ActorSystem system = ActorSystem.create("OperationSimulation");
+        final ActorRef theatre = system.actorOf(Props.create(OperationTheater.class),"OperationTheatre");
+        final ExecutionContext ec = system.dispatcher();
+
+        // Grab the controls
+        CompletableFuture<Object> heartLungMachineF = ask(theatre, new PassMeTheMachine() {
+        }, Timeout.apply(5, SECONDS)).toCompletableFuture();
+
+        final ActorRef heartLungMachine = (ActorRef) heartLungMachineF.get(5, SECONDS);
+
+        system.scheduler().scheduleOnce(Duration.apply(200, MILLISECONDS),
+                heartLungMachine, new PullBack(1f), ec, ActorRef.noSender()
+        );
+
+        system.scheduler().scheduleOnce(Duration.apply(1, SECONDS),
+                heartLungMachine, new PullBack(0f), ec, ActorRef.noSender()
+        );
+
+        system.scheduler().scheduleOnce(Duration.apply(2, SECONDS),
+                heartLungMachine, new PullBack(0.5f), ec, ActorRef.noSender()
+        );
+
+        system.scheduler().scheduleOnce(Duration.apply(3, SECONDS),
+                heartLungMachine, new PullBack(0f), ec, ActorRef.noSender()
+        );
+
+        // Shut down
+        system.scheduler().scheduleOnce(Duration.apply(3, SECONDS),
+                () -> system.shutdown(), ec
+        );
+
+    }
+}
---
+++ src/main/java/com/knoldus/akka/health/Monitor.java
@@ -0,0 +1,95 @@
+package com.knoldus.akka.health;
+
+import akka.actor.AbstractLoggingActor;
+import akka.actor.ActorRef;
+import akka.japi.pf.ReceiveBuilder;
+import scala.PartialFunction;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.duration.Duration;
+import scala.runtime.BoxedUnit;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by knoldus on 2/1/17.
+ */
+public class Monitor extends AbstractLoggingActor {
+
+    // We need an "ExecutionContext" for the scheduler.  This
+    // Actor's dispatcher can serve that purpose.  The
+    // scheduler's work will be dispatched on this Actor's own
+    // dispatcher
+    private final ExecutionContext ec = context().dispatcher();
+
+    // The maximum ceiling of our heart in 'feet'
+    private final int maxHeartRate = 200;
+
+    // The maximum rate of climb for our heart in
+    // 'feet per minute'
+    private final int maxRateOfClimb = 10;
+
+    // The varying rate of climb depending on the luMonitorng capacity
+    private float rateOfClimb = 0f;
+
+    // Our current heartRate
+    private double heartRate = 0;
+
+    private long lastTick = System.currentTimeMillis();
+
+    // We need to periodically update our heart rate.  This
+    // scheduled message send will tell us when to do that
+    private Object ticker = context().system().scheduler()
+            .schedule(Duration.apply(100, TimeUnit.MILLISECONDS),
+                    Duration.apply(100, TimeUnit.MILLISECONDS), self(), new Tick() {
+                    }, ec, ActorRef.noSender());
+
+    // An internal message we send to ourselves to tell us
+    // to update our heartRate
+    public interface Tick {
+    }
+
+    @Override
+    public PartialFunction<Object, BoxedUnit> receive() {
+        return ReceiveBuilder
+                // Our rate of climb has changed
+                .match(RateChange.class, msg -> {
+                    // Truncate the range of 'amount' to [-1, 1]
+                    // before multiplying
+                    float minimum = Math.min(msg.getAmount(), 1.0f);
+                    float maximum = Math.max(minimum, -1.0f);
+                    rateOfClimb = maximum * maxRateOfClimb;
+
+                    log().info("monitor changed rate of climb to {}.", rateOfClimb);
+                })
+
+                .match(Tick.class, msg -> {
+                    long tick = System.currentTimeMillis();
+                    heartRate = heartRate + ((tick - lastTick) / 60000.0) * rateOfClimb;
+                    lastTick = tick;
+                }).build();
+    }
+
+    public static class RateChange {
+        private final float amount;
+
+        public RateChange(final float amount) {
+            this.amount = amount;
+        }
+
+        public float getAmount() {
+            return this.amount;
+        }
+    }
+}
+
+class MonitorUpdate {
+    private final float rate;
+
+    public MonitorUpdate(final float rate) {
+        this.rate = rate;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+}
